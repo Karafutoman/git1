f388794d7632cceb75fe14a28f1df1fc79808fc9
Andrey Starik <karafutoman@mail.ru>
2022-07-19 16:43:24 +1100

    All files write and added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        var SalesManager = new SalesManager(new int[]{15, 27, 30, 42, 54});
+        System.out.printf("Максимум от {15, 27, 30, 42, 54} = %d\n", SalesManager.max());
     }
 }
