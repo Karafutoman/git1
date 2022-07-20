65e811cbd3e60ef1086ec469dfc2ebdd4abb3562
Andrey Starik <karafutoman@mail.ru>
2022-07-20 16:08:56 +1100

    Change from int to long

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        var SalesManager = new SalesManager(new int[]{15, 27, 30, 42, 54});
+        var SalesManager = new SalesManager(new long[]{15, 27, 30, 42, 54});
         System.out.printf("Максимум от {15, 27, 30, 42, 54} = %d\n", SalesManager.max());
     }
 }
